87e2a7b02e3ff3819b173b19ac9b7e8bcb347733
Tyler Lugger <notnottyler@gmail.com>
2020-09-08 15:49:09 +0100

    object orient, add logging

--- main.py
+++ main.py
@@ -6,77 +6,108 @@ import tweepy
 import os
 import random
 import time
+import logging
+import logging.handlers
 
 # An official team on July 5th 1991
 ROCKIES_SIGN = "cancer"
 
-def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+class Rockiscope():
+    def __init__(self):
+        self.auth = None
+        self.api = None
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, 
-        wait_on_rate_limit_notify=True)
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        raise e
-    return api
+        # bot epoch
+        self.last_day_sent = datetime(2020, 9, 7, hour=12, tzinfo=tz.gettz('America/Denver'))
 
-def send_tweet(text):
-    api = create_api()
-    status = api.update_status(text)
+    def create_api(self):
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+        access_token = os.getenv("ACCESS_TOKEN")
+        access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-# Get todays horiscope for provided sign
-def get_horiscope(sign):
-    r = requests.post("https://aztro.sameerkumar.website?sign={}&day=today".format(sign))
-    return r.json()
+        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.API(self.auth, wait_on_rate_limit=True,
+            wait_on_rate_limit_notify=True)
+        try:
+            self.api.verify_credentials()
+            logging.info("Twitter credentials verified")
+        except Exception as e:
+            logging.error(e)
+            raise e
+        return self.api
 
-# Get data from nearly unusable MLB stats API
-def get_game_data():
-    now = datetime.now()
-    year = now.year
-    date = now.strftime("%Y-%m-%d")
-    url = "https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&season={0}&startDate={1}&endDate={1}&teamId=115&&scheduleTypes=games".format(year, date)
-    r = requests.get(url)
-    return r.json()
+    def send_tweet(self, text):
+        status = self.api.update_status(text)
+        if status:
+            logging.info("Created new tweet")
 
-def get_prediction(game_time, lucky_time, pct):
-    win = False
-    game_hour = game_time.hour
-    lucky_hour = int(lucky_time[0:-2])
-    if lucky_time.endswith("pm"):
-        lucky_hour += 12
-    
-    time_delta = abs(lucky_hour-game_hour)
-    if time_delta == 0:
-        return "W"
+    # Get todays horiscope for provided sign
+    def get_horiscope(self, sign):
+        r = requests.post("https://aztro.sameerkumar.website?sign={}&day=today".format(sign))
+        return r.json()
 
-    # don't ask
-    stonks = (float(pct) + (1/(time_delta)))/2
-    if random.random() < stonks:
-        win = True
-    
-    return "W" if win else "L"
+    # Get data from nearly unusable MLB stats API
+    def get_game_data(self):
+        now = datetime.now()
+        year = now.year
+        date = now.strftime("%Y-%m-%d")
+        url = "https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&season={0}&startDate={1}&endDate={1}&teamId=115&&scheduleTypes=games".format(year, date)
+        r = requests.get(url)
+        return r.json()
+
+    def get_prediction(self, game_time, lucky_time, pct):
+        win = False
+        game_hour = game_time.hour
+        lucky_hour = int(lucky_time[0:-2])
+        if lucky_time.endswith("pm"):
+            lucky_hour += 12
+
+        time_delta = abs(lucky_hour-game_hour)
+        if time_delta == 0:
+            return "W"
+
+        # don't ask
+        stonks = (float(pct) + (1/(time_delta)))/2
+        if random.random() < stonks:
+            win = True
+
+        return "W" if win else "L"
 
 # create a default with no games
 default_game = {
     "totalGames": 0
 }
 
-# bot epoch
-last_day_sent = datetime(2020, 9, 5, hour=12, tzinfo=tz.gettz('America/Denver'))
+# make logging
+handler = logging.handlers.WatchedFileHandler("./logs/rockiscope.log")
+formatter = logging.Formatter(logging.BASIC_FORMAT)
+handler.setFormatter(formatter)
+root = logging.getLogger()
+root.setLevel("INFO")
+root.addHandler(handler)
 
 # do bot thing forever
+r = Rockiscope()
+okay = False
+# continue trying to make the API
+while not okay:
+    try:
+        r.create_api()
+        okay = True
+        logging.info("Created Tweepy API")
+    except Exception as e:
+        okay = False
+        time.sleep(15)
+
 while True:
     time.sleep(5)
-    now = datetime.now()
-    if now.date() > last_day_sent.date():
-        if now.hour == last_day_sent.hour:
-            game_day = get_game_data().get('dates', [default_game])[0]
+    now = datetime.now().astimezone(tz.gettz('America/Denver'))
+    if now.date() > r.last_day_sent.date():
+        if now.hour == r.last_day_sent.hour:
+            logging.info("Ready to Tweet")
+            game_day = r.get_game_data().get('dates', [default_game])[0]
             if game_day['totalGames'] > 0 :
                 game = game_day['games'][0]
                 
@@ -94,14 +125,15 @@ while True:
 
                 message = "Game Time: {}\n\nHome: {}\nAway: {}".format(game_time, home, away)
                 
-                horiscope = get_horiscope(ROCKIES_SIGN)
+                horiscope = r.get_horiscope(ROCKIES_SIGN)
                 message += "\n\nHorisope: {}".format(horiscope['description'])
 
-                prediction = get_prediction(game_date_mst, horiscope['lucky_time'], pct)
+                prediction = r.get_prediction(game_date_mst, horiscope['lucky_time'], pct)
                 message += "\n\nOutcome Prediction: {}".format(prediction)
 
-                send_tweet(message)
+                logging.info("Sending: {}".format(message))
 
-            # update last_day_sent ts after tweet or checking for a game    
-            last_day_sent = now    
+                r.send_tweet(message)
 
+            # update last_day_sent ts after tweet or checking for a game
+            r.last_day_sent = now
